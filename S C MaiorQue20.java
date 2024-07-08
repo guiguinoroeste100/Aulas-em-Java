438b8e0164d3ea6be5c3dc2bd26f3ec07788f18e
guiguinoroeste100 <guilhermejosebrasil10@outlook.com.br>
2024-07-08 11:12:48 -0300

    Add files via upload

---
+++ S C MaiorQue20.java
@@ -0,0 +1,26 @@
+package entities;
+
+import java.util.Scanner;
+
+public class MaiorQue20 {
+
+	public static void main(String[] args) {
+		{ Scanner input = new
+				Scanner(System.in);
+		}
+		{ int num = 0;
+		System.out.print("Digite um numero:");
+		if ( num >= 20) {
+			System.out.print("numero" +num+ "é maior que 20");
+		}else {
+				if( num < 20 ) {
+					System.out.print("numero " +num+" é maior que 20");
+				}
+				}
+		}
+		
+
+
+	}
+
+}
