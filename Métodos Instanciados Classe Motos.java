837bd61293621285693bb5b446125865912101ff
Guilherme José <guilhermejosebrasil10@outlook.com.br>
2024-07-13 10:53:21 -0300

    Add files via upload

---
+++ Métodos Instanciados Classe Motos.java
@@ -0,0 +1,23 @@
+package aplication;
+
+public class Moto {
+
+	public static void main(String[] args) {
+
+	}
+
+	public String cor;
+	public String marca;
+	public String modelo;
+	public String nroChassi;
+	public String nroCilindradas;
+	public String nroPlaca;	
+	public void licenciar() {		
+	}
+
+	public void status() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
