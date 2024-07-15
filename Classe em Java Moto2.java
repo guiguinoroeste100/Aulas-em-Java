5306a98497727e8fa389c2d56da8b816adc8c37b
Guilherme José <guilhermejosebrasil10@outlook.com.br>
2024-07-14 22:37:03 -0300

    Add files via upload

---
+++ Classe em Java Moto2.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Moto2 {
+
+	public static void main(String[] args) {
+		String modelo, marca, nroChassi, nroPlaca, cor;
+		boolean licenciada;}
+	int nroCilindradas;
+		
+		
+		void licenciar() {
+		}{
+			boolean licenciada = true;
+			
+		}
+		void verificaLicenca(){
+		
+	}
+
+}
