d5468cada5f9683b31938262657708dd0419a779
Guilherme José <guilhermejosebrasil10@outlook.com.br>
2024-07-16 18:47:58 -0300

    Add files via upload

---
+++ Encapsulamento em Java.java
@@ -0,0 +1,15 @@
+package Application;
+
+public class main {
+
+	public static void main(String[] args) {
+		Estudante el = new Estudante();
+		el.setNome("Joaozinho");
+		String nomEstudante = el.getNome(); {
+			// TODO Auto-generated method stub
+			System.out.println(nomEstudante);
+		}
+
+	}
+
+}
