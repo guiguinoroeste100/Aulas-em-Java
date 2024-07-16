df37bc99a5a3c9ebd607a68563119d6766ed1339
Guilherme José <guilhermejosebrasil10@outlook.com.br>
2024-07-16 14:39:11 -0300

    Add files via upload

---
+++ Encapsulamento e Métodos Modificadores Area.java
@@ -0,0 +1,43 @@
+package Application;
+
+public class Area {
+
+	public static float main(String[] args) {
+		try {
+			try {
+				return area;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return area;
+		
+
+	}
+	private static float area;
+	private float raio;
+	private String cor;
+	public void setArea(float
+			area) { this.area = area;
+	}
+	public double
+	getRaio() { return
+			raio;
+	}
+	public void setRaio(float
+			raio) { this.raio = raio;
+	}
+	public String
+	getCor() { return
+			getCor(); }
+	public void setCor(String
+			cor) { this.cor = cor;
+	}
+			}
+	
+
+	
