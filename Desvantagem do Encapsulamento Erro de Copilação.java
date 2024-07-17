a727f07443d9970c25f8b906c7401ce7d267ef55
Guilherme José <guilhermejosebrasil10@outlook.com.br>
2024-07-17 11:59:34 -0300

    Add files via upload

---
+++ Desvantagem do Encapsulamento Erro de Copilação.java
@@ -0,0 +1,12 @@
+package Application;
+
+public class main {
+
+	public static void main(String[] args) {
+		Estudante el = new Estudante ();
+		el.nome = "Joaozinho";
+		String nomEstudante = el.nome;
+
+	}
+
+}
